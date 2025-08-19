4e543a074b36bf93073e5e9ba18b145319bc62f4
gagurolle <stalocers@gmail.com>
2025-08-19 03:04:06 +0200

    kkk

--- SuzumesDeepDungeon/Migrations/DatabaseContextModelSnapshot.cs
+++ SuzumesDeepDungeon/Migrations/DatabaseContextModelSnapshot.cs
@@ -56,7 +56,7 @@ namespace SuzumesDeepDungeon.Migrations
 
                     b.HasIndex("GameId");
 
-                    b.ToTable("Achievements");
+                    b.ToTable("Achievements", (string)null);
                 });
 
             modelBuilder.Entity("SuzumesDeepDungeon.Models.GameRank", b =>
@@ -116,7 +116,7 @@ namespace SuzumesDeepDungeon.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("GameRanks");
+                    b.ToTable("GameRanks", (string)null);
                 });
 
             modelBuilder.Entity("SuzumesDeepDungeon.Models.GameTag", b =>
@@ -164,7 +164,7 @@ namespace SuzumesDeepDungeon.Migrations
 
                     b.HasIndex("GameId");
 
-                    b.ToTable("Tag");
+                    b.ToTable("Tag", (string)null);
                 });
 
             modelBuilder.Entity("SuzumesDeepDungeon.Models.Screenshot", b =>
@@ -205,7 +205,7 @@ namespace SuzumesDeepDungeon.Migrations
                     b.HasIndex("GameId")
                         .IsUnique();
 
-                    b.ToTable("Screenshots");
+                    b.ToTable("Screenshots", (string)null);
                 });
 
             modelBuilder.Entity("SuzumesDeepDungeon.Models.Stores", b =>
@@ -242,7 +242,7 @@ namespace SuzumesDeepDungeon.Migrations
 
                     b.HasIndex("GameId");
 
-                    b.ToTable("Stores");
+                    b.ToTable("Stores", (string)null);
                 });
 
             modelBuilder.Entity("SuzumesDeepDungeon.Models.Trailer", b =>
@@ -284,7 +284,7 @@ namespace SuzumesDeepDungeon.Migrations
 
                     b.HasIndex("GameId");
 
-                    b.ToTable("Trailers");
+                    b.ToTable("Trailers", (string)null);
                 });
 
             modelBuilder.Entity("SuzumesDeepDungeon.Models.User", b =>
@@ -323,7 +323,7 @@ namespace SuzumesDeepDungeon.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Users");
+                    b.ToTable("Users", (string)null);
                 });
 
             modelBuilder.Entity("SuzumesDeepDungeon.Models.GameAchievement", b =>
