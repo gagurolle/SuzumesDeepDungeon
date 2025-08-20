e0337572864912c0b7a026ebda9fb777d9486f20
gagurolle <stalocers@gmail.com>
2025-08-20 21:01:07 +0200

    debugging

--- SuzumesDeepDungeon/Program.cs
+++ SuzumesDeepDungeon/Program.cs
@@ -39,6 +39,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+Console.WriteLine(builder.Configuration["rawgAPI"]);
 RawgApi.rawgApi = builder.Configuration["rawgAPI"] ?? "";
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowAll", policy => {
