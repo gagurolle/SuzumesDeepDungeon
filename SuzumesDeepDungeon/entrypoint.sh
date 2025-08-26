bbb4a48c7981788433bf7507ff91aa80202c4374
gagurolle <stalocers@gmail.com>
2025-08-26 21:42:49 +0200

    added entrypoint

--- SuzumesDeepDungeon/entrypoint.sh
+++ SuzumesDeepDungeon/entrypoint.sh
@@ -4,15 +4,6 @@
 echo "Waiting for PostgreSQL to be ready..."
 sleep 5
 
-echo "Checking if database exists..."
-if ! PGPASSWORD="130171" psql -h db -U postgres -lqt | cut -d \| -f 1 | grep -qw gameDB; then
-    echo "Database does not exist. Creating database..."
-    PGPASSWORD="130171" createdb -h db -U postgres gameDB
-    echo "Database created successfully."
-else
-    echo "Database already exists."
-fi
-
 # Применяем миграции
 echo "Applying database migrations..."
 dotnet ef database update --project /app/src/SuzumesDeepDungeon.csproj
